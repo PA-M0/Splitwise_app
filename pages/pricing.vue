76ca3b5cce7e138ee2285c041c54e581c45f80ba
ParsaShafagh <108547105+PA-M0@users.noreply.github.com>
2024-03-27 05:06:49 +0330

    pricing is updated to working ot it .

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -1,33 +1,48 @@
 <template>
  
+  
+ <header>
+    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <div class="flex justify-between items-center mx-auto max-w-screen-xl">
+
+
+
+        <form>
+          <button type="submit" class="p-1.5 ">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+            <span class="sr-only">Search</span>
+          </button>
+        </form>
+
+
+
+        <a href="#" class="text-mygreenH  dark:text-white hover:text-green-100">Add friends</a>
+
+
+
+      </div>
+    </nav>
+  </header>
+
+
   <!-- TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com -->
   <hr
-    class="my-3 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
+    class=" h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
 
-  <div style="border-style: solid;border-radius: 5px; border-width: 1px; background-color: #ededee;">
-    <h1 style="font-size: 24px;" class="text-center p-3 text-dark">
-      <img src="../assets/images/user.png" alt="" class="m-1" style="display: inline-block;" width="20">Pricing
-    </h1>
+    <div class="bg-gray-100" style="border-style: solid; border-width: 1px; ">
+      <h1 style="font-size: 20px;" class=" p-1 text-gray-800">
+        <img src="../assets/images/user.png" alt="" class="" style="display: inline-block;" width="20">Pricing
+      </h1>
 
-  </div>
-
-
+    </div>
 
 
-  <!-- <button type="button" style="" @click="aa()"
-    class="text-white float-right block bg-blue-700 mt-2 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add
-    expense
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: inline-block;" x="0px" y="0px" width="15" height="15"
-      viewBox="0,0,256,256">
-      <g fill="#ffffff" fill-rule="evenodd" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter"
-        stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none"
-        font-size="none" text-anchor="none" style="mix-blend-mode: normal">
-        <g transform="scale(10.66667,10.66667)">
-          <path d="M11,2v9h-9v2h9v9h2v-9h9v-2h-9v-9z"></path>
-        </g>
-      </g>
-    </svg></button> -->
-    <p>I'm working on it</p>
+  
+    <p>i'm working on it</p>
   <br><br><br><br><br><br><br><br><br><br><br><br>
   <br><br><br><br><br><br><br><br><br><br><br><br>
   
