a1571711a3cd174aa204635d8c04b0d5dab05753
ParsaShafagh <108547105+PA-M0@users.noreply.github.com>
2024-03-27 05:36:27 +0330

    dialog is updated.

--- components/AddFriendBtn.vue
+++ components/AddFriendBtn.vue
@@ -27,29 +27,31 @@
 
 
     <FriendList />
-  
+
 
     <v-row justify="end" class="mt-2">
         <v-dialog v-model="dialog" persistent max-width="450">
             <template v-slot:activator="{ props }">
-                <v-btn class="text-none m-auto" size="large" variant="outline" style="border: #309178 solid 2px;border-color: #309178; color:#20543F; border-radius:5px; font-size:12px;" v-bind="props">
+                <v-btn class="text-none m-auto" size="large" variant="outline"
+                    style="border: #309178 solid 2px;border-color: #309178; color:#20543F; border-radius:5px; font-size:12px;"
+                    v-bind="props">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-5 h-5 inline-block mr-1 text-mygreenH">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
                     </svg>
                     <span class="font-bold text-mygreenH"><b>Add more friends</b></span>
-                   
+
                 </v-btn>
             </template>
             <v-card class="max-w-md">
                 <v-card-title class="text-center ">
-                    <v-btn style="color:#58C3A7" class="text-none mr-5  text-sm" variant="text" @click="dialog = false">
+                    <v-btn style="color:#309178" class="text-none mr-5  text-sm" variant="text" @click="dialog = false">
                         Cancel
                     </v-btn>
 
                     <span class="text-md"><strong>Add friends </strong></span>
-                    <v-btn style="color:#58C3A7" class="text-none ml-5 text-sm" variant="text" @click="dialog = false">
+                    <v-btn style="color:#309178" class="text-none ml-5 text-sm" variant="text" @click="dialog = false">
                         Next
                     </v-btn>
 
@@ -70,19 +72,42 @@
                                             <v-row>
                                                 <v-col>
                                                     <form @submit.prevent="search_friend_by_email()"
-                                                        class="max-w-sm mx-auto">
+                                                        class="max-w-sm p-1">
                                                         <div class="mb-5 mt-3">
-                                                            <label for="email"
-                                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white ">Your
-                                                                email</label>
-                                                            <input type="email" v-model="formData.email" id="email"
-                                                                class=" bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-darken-2 focus:border-green-700 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                                placeholder="example@gmail.com" required>
+                                                            <label for="email-address-icon"
+                                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                                                Email</label>
+                                                            <div class="relative">
+                                                                <div
+                                                                    class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                                                                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                                                                        aria-hidden="true"
+                                                                        xmlns="http://www.w3.org/2000/svg"
+                                                                        fill="currentColor" viewBox="0 0 20 16">
+                                                                        <path
+                                                                            d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+                                                                        <path
+                                                                            d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+                                                                    </svg>
+                                                                </div>
+                                                                <input type="email" v-model="formData.email" id="email"
+                                                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                                    placeholder="example@gmail.com" required>
+                                                            </div>
+
+
                                                         </div>
 
 
-                                                        <button type="submit"
-                                                            class="text-white bg-mygreenH hover:bg-mygreenH focus:ring-4 focus:outline-none focus:ring-teal-darken-2 font-medium rounded-lg text-sm  px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                                                        <button type="submit" style="text-align: right;"
+                                                            class="text-white bg-mygreen hover:bg-mygreenH focus:ring-2 focus:outline-none focus:ring-mygreenH font-medium rounded-lg text-sm  px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                                                        <p id="helper-text-explanation"
+                                                            class="mt-2 text-xs text-gray-500 dark:text-gray-400">We’ll
+                                                            never share
+                                                            your details. Read our <a href="#"
+                                                                class="font-medium text-blue-600 hover:underline dark:text-blue-500">Privacy
+                                                                Policy</a>.</p>
+
                                                     </form>
                                                 </v-col>
 
@@ -93,19 +118,43 @@
                                         <v-window-item value="two">
                                             <v-row>
                                                 <v-col>
-                                                    <form @submit.prevent="submitForm" class="max-w-sm mx-auto">
 
+                                                    <form @submit.prevent="submitForm" class="max-w-sm mx-auto p-1">
+                                                        
                                                         <div class="mb-5 mt-3">
                                                             <label for="tel"
                                                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                                                phone</label>
-                                                            <input type="tel" v-model="formData.phone" id="phone"
-                                                                class="form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                                placeholder="09123456789" required>
+                                                                Phone</label>
+                                                            <div class="relative">
+                                                                <div
+                                                                    class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                        viewBox="0 0 24 24" stroke-width="1.5"
+                                                                        stroke="currentColor" class="w-5 h-5">
+                                                                        <path stroke-linecap="round"
+                                                                            stroke-linejoin="round"
+                                                                            d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
+                                                                    </svg>
+
+                                                                </div>
+                                                                <input type="tel" v-model="formData.phone" id="phone"
+                                                                    class="bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                                    placeholder="09123456789" required>
+                                                            </div>
+
+
                                                         </div>
 
+
                                                         <button type="submit"
-                                                            class="text-white bg-mygreenH hover:bg-mygreenH focus:ring-4 focus:outline-none focus:ring-teal-darken-2 font-medium rounded-lg text-sm  px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                                                            class="text-white bg-mygreen hover:bg-mygreenH focus:ring-4 focus:outline-none focus:ring-teal-darken-2 font-medium rounded-lg text-sm  px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+
+                                                        <p id="helper-text-explanation"
+                                                            class="mt-2 text-xs text-gray-500 dark:text-gray-400">We’ll
+                                                            never share
+                                                            your details. Read our <a href="#"
+                                                                class="font-medium text-blue-600 hover:underline dark:text-blue-500">Privacy
+                                                                Policy</a>.</p>
                                                     </form>
                                                 </v-col>
 
